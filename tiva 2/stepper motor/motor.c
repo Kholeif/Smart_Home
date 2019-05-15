7a53cae611bc13f9832de43a9ca3cea8d81d1026
muhamed <muhamed.swelam@gmail.com>
2019-05-15 15:48:10 +0200

    motor commit

---
+++ tiva 2/stepper motor/motor.c
@@ -0,0 +1,26 @@
+#include "motor.h"
+#include "port.h"
+#include "dio.h"
+
+//p0,p1,p4,p5
+//0x01,0x02,0x10,0x20
+extern uint8_t cursor;
+
+void _delay_ms(uint32_t y)
+{
+	 unsigned long volatile t;
+	 t= 1000*y;
+ 	 while(t){t--;}
+ }
+
+ 
+ void stepper_setup()
+ {
+	 Port_Init(1);
+	 Port_Init(5);
+	 Port_SetPinDirection(1,0x33,PORT_PIN_OUT);
+	 Port_SetPinDirection(5,0x11,PORT_PIN_IN);
+	 DIO_WritePort (1,0x33,STD_LOW);
+	 Port_SetPinPullUp(5,0x11,1);
+ }
+ 
---
+++ tiva 2/stepper motor/motor.h
@@ -0,0 +1,6 @@
+#include "micro_config.h"
+
+void _delay_ms(uint32_t y);
+void movestepperright();
+void movestepperleft();
+void stepper_setup();
